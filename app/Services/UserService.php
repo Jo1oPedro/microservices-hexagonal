e1af48b6fa8f83dba77f872c131313482cfd03b2
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-05-10 18:54:16 -0300

    refactor: refactor user service to use create user output and user repository to create the new user

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
 
-use App\DTO\UserDTO;
-use App\Model\User;
+use App\Domain\User\User as DomainUser;
 use App\Domain\User\UserRepository;
+use App\DTO\CreateUserInput;
+use App\DTO\CreateUserOutput;
 
 class UserService
 {
@@ -12,11 +15,21 @@ class UserService
          private UserRepository $userRepository
     ) {}
 
-    public function create(UserDTO $userDTO)
+    public function create(CreateUserInput $createUserInput): CreateUserOutput
     {
-        $user = new User($userDTO->toArray());
+        $passwordHash = password_hash($createUserInput->password, PASSWORD_BCRYPT);
+
+        $user = DomainUser::create($createUserInput->name, $createUserInput->email, $passwordHash);
+        $user = $this->userRepository->save($user);
+
         //$emailWelcome = new WelcomeEmail();
         //$emailWelcome->sendEmail($user->email);
-        return $this->userRepository->save($user);
+        return new CreateUserOutput(
+            id: $user->id,
+            name: $user->name,
+            email: $user->email,
+            createdAt: $user->createdAt->format(\DateTimeInterface::ATOM),
+            updatedAt: $user->updatedAt->format(\DateTimeInterface::ATOM),
+        );
     }
 }
