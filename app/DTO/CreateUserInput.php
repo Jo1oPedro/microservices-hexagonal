cc1ae9fad3284ae651b336d26581da3b52e2fc20
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-05-10 18:53:01 -0300

    feat: replace UserDTO with CreateUserInput and CreateUserOutput

---
+++ app/DTO/CreateUserInput.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DTO;
+
+class CreateUserInput
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $email,
+        public readonly string $password
+    ) {}
+
+    public function toArray()
+    {
+        return [
+            "name" => $this->name,
+            "email" => $this->email,
+            "password" => $this->password
+        ];
+    }
+}
