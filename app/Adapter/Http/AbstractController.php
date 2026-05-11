534a9988b4a1fbae5f102746f1abc433cbdf0469
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-05-11 15:18:44 -0300

    refactor: fix base abstract controller layer, changing it to the http layer of the adapter folder

--- app/Adapter/Http/AbstractController.php
+++ app/Adapter/Http/AbstractController.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
-namespace app\Adapter;
+namespace app\Adapter\Http;
 
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Contract\RequestInterface;
