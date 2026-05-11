84115a6eb93a81a8a3fdf17b3825a2677fc3c704
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-05-11 18:52:14 -0300

    fix: fix welcome email message queue exchange name

--- main-app/app/Infrastructure/User/Notification/Message/WelcomeEmailMessage.php
+++ main-app/app/Infrastructure/User/Notification/Message/WelcomeEmailMessage.php
@@ -5,7 +5,7 @@ namespace app\Infrastructure\User\Notification\Message;
 use Hyperf\Amqp\Annotation\Producer;
 use Hyperf\Amqp\Message\ProducerMessage;
 
-#[Producer(exchange: "notifcations", routingKey: "user.welcome_email")]
+#[Producer(exchange: "notifications", routingKey: "user.welcome_email")]
 class WelcomeEmailMessage extends ProducerMessage
 {
     public function __construct(string $email)
